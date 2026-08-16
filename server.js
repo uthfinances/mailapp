4d9610812fba4442f132568f9ddfceb7a6d07840
uthfinances <uthfinances@gmail.com>
2026-08-16 19:00:11 +0100

    Modify CSP to include unsafe-inline for scripts
    Updated Content Security Policy to allow unsafe inline scripts.

--- server.js
+++ server.js
@@ -269,7 +269,7 @@ app.use(
     contentSecurityPolicy: {
       directives: {
         defaultSrc: ["'self'"],
-        scriptSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
         styleSrc: ["'self'", "'unsafe-inline'"],
         imgSrc: ["'self'", 'data:'],
       },
